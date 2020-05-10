e2a78bb03e7d5f6614460c850f2b97c47d25ce78
lucasnereslcs <lucasnereslcs@gmail.com>
2020-05-09 21:29:28 -0300

    Projeto Criado

---
+++ src/aplicacao.java
@@ -0,0 +1,14 @@
+
+public class aplicacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Cliente c1 = new Cliente("Lucas", "lucasnereslcs@hotmail.com");
+		Cliente c2 = new Cliente("Maria", "maria@gmail.com");
+
+		System.out.println(c1.equals(c2));
+
+	}
+
+}
